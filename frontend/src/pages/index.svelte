af5c82967b01384d33fc4018fffcc41aa6486bf6
Kaelan Moffett-Steinke <kaelan.ms@gmail.com>
2021-04-19 21:18:49 -0400

    remove temp sass crap

--- frontend/src/pages/index.svelte
+++ frontend/src/pages/index.svelte
@@ -5,31 +5,5 @@
   <Homepage/>
 </main>
 
-<style lang='scss'>
-  main{
-    margin: 0;
-    padding: 0;
-    width: auto;
-    height: auto;
-    color: red;
-  }
-	/* main { */
-		/* text-align: center; */
-		/* padding: 1em; */
-		/* max-width: 240px; */
-		/* margin: 0 auto; */
-	/* } */
-
-	/* h1 { */
-		/* color: #ff3e00; */
-		/* text-transform: uppercase; */
-		/* font-size: 4em; */
-		/* font-weight: 100; */
-	/* } */
-
-	/* @media (min-width: 640px) { */
-		/* main { */
-			/* max-width: none; */
-		/* } */
-	/* } */
+<style>
 </style>
