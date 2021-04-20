284a32ac12a4f6f423dab4ff1c8804aa39677632
Krishn <krishnkiran.ray@gmail.com>
2021-04-19 21:06:25 -0400

    added basic functionality for resume input form

---
+++ frontend/src/components/resume_input_form/points.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let points;
+
+    const addPoint = () => {
+        points = [...points, {point: '', keywords: []}];
+    }
+
+    const addKeyword = (point) => {
+        point.keywords = [...point.keywords, ''];
+    } 
+</script>
+
+<div class='points'>
+    {#each points as point}
+        <input type='text' bind:value={point.text}/>
+        {#each point.keyword as keyword}
+            <input type='text' bind:value={keyword}/>
+        {/each}
+        <button on:click={()=>{addKeyword(point)}}>
+            Add Keyword
+        </button>   
+    {/each}
+    <button on:click={addPoint}>
+        Add Point
+    </button>
+</div>
