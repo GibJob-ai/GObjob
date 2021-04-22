865982c41671c6e705bd67c3975e103699f52beb
Ryan Prairie <me@ryanprairie.com>
2021-04-22 14:45:45 -0400

    db config hooked up

--- config/config.go
+++ config/config.go
@@ -7,7 +7,17 @@ import (
 
 // the base of the struct that the data goes into
 type Config struct {
-	Mode string `hcl:"Mode"`
+	Mode string   `hcl:"Mode"`
+	DB   DBConfig `hcl:"DB,block"`
+}
+
+type DBConfig struct {
+	Host     string `hcl:"Host"`
+	Port     string `hcl:"Port"`
+	User     string `hcl:"User"`
+	Password string `hcl:"Password"`
+	DBName   string `hcl:"DBName"`
+	SSLMode  string `hcl:"SSLMode"`
 }
 
 // a global variable to be able to access anywhere
@@ -18,5 +28,11 @@ func Load() {
 	err := hclsimple.DecodeFile("config.hcl", nil, &CONFIG)
 	if err != nil {
 		log.Fatalf("Failed to load config: %s", err)
+	}
+
+	// if its in dev mode
+	// print out entire config
+	if CONFIG.Mode == "dev" {
+		log.Printf("CONFIG: %#v", CONFIG)
 	}
 }
--- db/db.go
+++ db/db.go
@@ -2,9 +2,11 @@ package db
 
 import (
 	// "os"
-	"github.com/jinzhu/gorm"
-  // "fmt"
+	"fmt"
 
+	"github.com/jinzhu/gorm"
+
+	"github.com/GibJob-ai/GObjob/config"
 	// gorm postgres dialect
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -17,7 +19,20 @@ type DB struct {
 // ConnectDB : connecting DB
 func ConnectDB() (*DB, error) {
 
-	db, err := gorm.Open("postgres", "host=localhost port=5432 user=gibjob dbname=gibjob password=gibjob sslmode=disable")
+	// format them all to a string
+	dboptions := fmt.Sprintf(
+		"host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		config.CONFIG.DB.Host,
+		config.CONFIG.DB.Port,
+		config.CONFIG.DB.User,
+		config.CONFIG.DB.DBName,
+		config.CONFIG.DB.Password,
+		config.CONFIG.DB.SSLMode,
+	)
+
+	// connect to db
+	db, err := gorm.Open("postgres", dboptions)
+	//db, err := gorm.Open("postgres", "host=localhost port=5432 user=gibjob dbname=gibjob password=gibjob sslmode=disable")
 
 	if err != nil {
 		panic(err)
