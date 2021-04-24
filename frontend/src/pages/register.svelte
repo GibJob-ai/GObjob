28af791ecaab9a133a15246e88e33e160a4b3777
Kaelan Moffett-Steinke <kaelan.ms@gmail.com>
2021-04-24 15:09:56 -0400

    add initial register page

---
+++ frontend/src/pages/register.svelte
@@ -0,0 +1,8 @@
+<script>
+  import FullScreenFlexPage from '../components/general/fullscreen_flex_page/fullscreen_flex_page.svelte'
+  import RegisterPage from '../components/register_page/register_page.svelte'
+</script>
+<FullScreenFlexPage>
+  <RegisterPage/>
+</FullScreenFlexPage>
+
