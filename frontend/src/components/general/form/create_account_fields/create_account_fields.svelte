13d2542856db89e860e7008e375a707416c1a008
Ryan Prairie <me@ryanprairie.com>
2021-04-27 19:09:22 -0400

    added baby signup mutation

--- frontend/src/components/general/form/create_account_fields/create_account_fields.svelte
+++ frontend/src/components/general/form/create_account_fields/create_account_fields.svelte
@@ -5,48 +5,51 @@
 	email.set(''); // null out the store after taking the value so that if they reload the page they start over
 	let form;
 
-	import { mutation } from 'svelte-apollo';
-	import gql from 'graphql-tag'
-	const signupQuery = gql`
-		mutation{
-			signUp(
+	import { mutation } from '@urql/svelte';
+
+
+	const signupMutation = mutation({
+			query: `
+			mutation($email: String!, $password: String!, $firstName: String!, $lastName: String!){
+				signUp(
 					email: $email,
 					password: $password,
 					firstName: $firstName,
 					lastName: $lastName
-			){
-				ok,
-				error,
+				){
+					ok,
+					error,
+				}
 			}
-		}
-	`;
+		`,
+	});
 
-	const signUp = mutation(signupQuery);
-	const submit = async () => {
-			try{
-					data = await signUp({ variables:
-											 {email: "test1@test.com",
-												password: "test",
-												firstName: "test",
-												lastName: "test"}})
-					if(data.data){
-							console.log(data.data)
-					}
-			} catch(error){
-					console.error("graphql error: " + error)
+	const signUp = mutation(signupMutation);
+	const submit = () => {
+		signupMutation({
+			email: "test10@test.com",
+			password: "test",
+			firstName: "test",
+			lastName: "test"
+		}).then(result => {
+			if(result.data){
+				console.log(result.data);
+			} else if (result.error){
+				console.error(result.error);
 			}
+		});
 	};
 
 </script>
 <style src='create_account_fields.scss'>
 </style>
 
-<form id='registerForm' bind:this={form}>
+<form id='registerForm' bind:this={form} on:submit|preventDefault={submit}>
 	<div id=labledInputsBlock>
 		{#each inputs as input}
 			<LabledTextInput label={input.name} bind:value={input.val} bind:form={form}/>
 		{/each}
-		<button type='submit' value='Submit' id='registerButton' on:click={submit} on:submit|preventDefault={()=>{}}>
+		<button type='submit' value='Submit' id='registerButton'>
 			Register
 		</button>
 	</div>
