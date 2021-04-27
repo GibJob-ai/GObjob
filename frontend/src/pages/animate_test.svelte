d67ad051a18b0c3b0cdfd9cac54170d9b4104f80
Kaelan Moffett-Steinke <kaelan.ms@gmail.com>
2021-04-26 21:12:19 -0400

    add test button

---
+++ frontend/src/pages/animate_test.svelte
@@ -0,0 +1,8 @@
+<script>
+	// import FullScreenFlexPage from '../components/general/fullscreen_flex_page/fullscreen_flex_page.svelte';
+	import AnimateTestPage from '../components/animate/test_page/test_page.svelte';
+</script>
+<!-- <FullScreenFlexPage> -->
+<div style="display: block; height: 100vh;">
+	<AnimateTestPage/>
+</div>
