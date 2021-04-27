8cf83c9b1a180a32b107da3dafd426d16676b463
Ryan Prairie <me@ryanprairie.com>
2021-04-27 17:54:39 -0400

    trying to get submit login mutation working

--- frontend/src/components/general/form/create_account_fields/create_account_fields.svelte
+++ frontend/src/components/general/form/create_account_fields/create_account_fields.svelte
@@ -3,8 +3,40 @@
 	import {email} from '../../../../store/register.js';
 	let inputs = [{'name':'Username', 'val': ''},{'name':'Email', 'val': $email},{'name':'Password', 'val': ''}];
 	email.set(''); // null out the store after taking the value so that if they reload the page they start over
-	const submit = () => {};
 	let form;
+
+	import { mutation } from 'svelte-apollo';
+	import gql from 'graphql-tag'
+	const signupQuery = gql`
+		mutation{
+			signUp(
+					email: $email,
+					password: $password,
+					firstName: $firstName,
+					lastName: $lastName
+			){
+				ok,
+				error,
+			}
+		}
+	`;
+
+	const signUp = mutation(signupQuery);
+	const submit = async () => {
+			try{
+					data = await signUp({ variables:
+											 {email: "test1@test.com",
+												password: "test",
+												firstName: "test",
+												lastName: "test"}})
+					if(data.data){
+							console.log(data.data)
+					}
+			} catch(error){
+					console.error("graphql error: " + error)
+			}
+	};
+
 </script>
 <style src='create_account_fields.scss'>
 </style>
@@ -14,7 +46,7 @@
 		{#each inputs as input}
 			<LabledTextInput label={input.name} bind:value={input.val} bind:form={form}/>
 		{/each}
-		<button type='submit' value='Submit' id='registerButton' on:click={submit}>
+		<button type='submit' value='Submit' id='registerButton' on:click={submit} on:submit|preventDefault={()=>{}}>
 			Register
 		</button>
 	</div>
