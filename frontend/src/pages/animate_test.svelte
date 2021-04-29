40bc1b7c0b7e5deea7b20f0c7a9b078570cba94a
Kaelan Moffett-Steinke <kaelan.ms@gmail.com>
2021-04-28 22:14:56 -0400

    remove animation test page

--- frontend/src/pages/animate_test.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	// import FullScreenFlexPage from '../components/general/fullscreen_flex_page/fullscreen_flex_page.svelte';
-	import AnimateTestPage from '../components/animate/test_page/test_page.svelte';
-</script>
-<!-- <FullScreenFlexPage> -->
-<div style="display: block; height: 100vh;">
-	<AnimateTestPage/>
-</div>
