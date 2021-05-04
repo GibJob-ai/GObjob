8b204bba0669a0f6469905b036bb31364c211cd4
Kaelan Moffett-Steinke <kaelan.ms@gmail.com>
2021-05-04 14:06:44 -0400

    add toggle WIP

---
+++ frontend/src/components/general/form/labled_toggle/labled_toggle.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Toggle from '../toggle/toggle.svelte';
+	export let on;
+	export let title;
+</script>
+<style src='labled_toggle.scss'>
+</style>
+<div class='labled_toggle'>
+	<div class='title'>{title}</div>
+	<Toggle bind:on/>
+</div>
