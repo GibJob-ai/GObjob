62140e38101c895f34cd7ac553f302a88e946353
Kaelan Moffett-Steinke <kaelan.ms@gmail.com>
2021-05-04 15:46:11 -0400

    fix indent

--- frontend/src/pages/file_upload.svelte
+++ frontend/src/pages/file_upload.svelte
@@ -1,5 +1,5 @@
 <script>
-  import FullScreenFlexPage from '../components/general/fullscreen_flex_page/fullscreen_flex_page.svelte'
+	import FullScreenFlexPage from '../components/general/fullscreen_flex_page/fullscreen_flex_page.svelte'
 	import FileUploadPage from '../components/file_upload_page/file_upload_page.svelte'
 </script>
 <FullScreenFlexPage>
