eee9f3e3f8a27f04cfb88da5572e6f0082ffed8f
Ryan Prairie <me@ryanprairie.com>
2021-05-05 15:20:04 -0400

    user sign up cleaned up

--- resolvers/sign_up.go
+++ resolvers/sign_up.go
@@ -1,6 +1,7 @@
 package resolvers
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/GibJob-ai/GObjob/model"
@@ -8,7 +9,7 @@ import (
 )
 
 // SignUp mutation creates user
-func (r *Resolvers) SignUp(args signUpMutationArgs) (*SignUpResponse, error) {
+func (r *Resolvers) SignUp(args signUpMutationArgs) (*UserResponse, error) {
 
 	// hash the password
 	hashPassword, err := utils.HashPass(args.Password)
@@ -19,35 +20,36 @@ func (r *Resolvers) SignUp(args signUpMutationArgs) (*SignUpResponse, error) {
 
 	newUser := model.User{Email: args.Email, Password: hashPassword, Username: args.Username}
 
+	// if user already exists return an error of UserExists
 	if !r.DB.Where("email = ?", args.Email).First(&model.User{}).RecordNotFound() {
-		msg := "Already signed up"
-		return &SignUpResponse{Status: false, Msg: &msg, User: nil}, nil
+		return nil, &userExistsError{Code: "UserExists", Message: "User already signed up"}
 	}
 
 	r.DB.Create(&newUser)
 
-	return &SignUpResponse{Status: true, Msg: nil, User: &UserResponse{u: &newUser}}, nil
+	return &UserResponse{u: &newUser}, nil
 }
 
+// arguements
 type signUpMutationArgs struct {
 	Email    string
 	Password string
 	Username string
 }
 
-// SignUpResponse is the response type
-type SignUpResponse struct {
-	Status bool
-	Msg    *string
-	User   *UserResponse
+// user exists error
+type userExistsError struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
 }
 
-// Ok for SignUpResponse
-func (r *SignUpResponse) Ok() bool {
-	return r.Status
+func (e userExistsError) Error() string {
+	return fmt.Sprintf("error [%s]: %s", e.Code, e.Message)
 }
 
-// Error for SignUpResponse
-func (r *SignUpResponse) Error() *string {
-	return r.Msg
+func (e userExistsError) Extensions() map[string]interface{} {
+	return map[string]interface{}{
+		"code":    e.Code,
+		"message": e.Message,
+	}
 }
